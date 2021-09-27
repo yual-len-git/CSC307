77e378e7a12b4541781f82b150d3bc0a691dfa47
yual-len-git <allenyu90@gmail.com>
2021-09-27 12:07:45 -0700

    final

--- react-assignment1/src/Form.js
+++ react-assignment1/src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState(
      {
         name: '',
@@ -26,28 +26,29 @@ function Form() {
         props.handleSubmit(person);
         setPerson({name: '', job: ''});
     }
-} 
-  
 
-return (
-    <form>
-        <label htmlFor="name">Name</label>
-        <input
-            type="text"
-            name="name"
-            id="name"
-            value={person.name}
-            onChange={handleChange} />
-        <label htmlFor="job">Job</label>
-        <input
-            type="text"
-            name="job"
-            id="job"
-            value={person.job}
-            onChange={handleChange} />
-    </form>
-);
+    // <input type="button" value="Submit" onClick={submitForm} />
 
+    return (
+        <form>
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                value={person.name}
+                onChange={handleChange} />
+            <label htmlFor="job">Job</label>
+            <input
+                type="text"
+                name="job"
+                id="job"
+                value={person.job}
+                onChange={handleChange} />
+            <input type="button" value="Submit" onClick={submitForm} />
+        </form>
+    );
+}
   
 
 export default Form;
