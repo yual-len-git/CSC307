d92afa17c1194a52f699593009566380ae5dbeb9
yual-len-git <allenyu90@gmail.com>
2021-09-27 11:58:39 -0700

    change

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 // const characters = [
 //     {
@@ -22,32 +23,38 @@ import Table from './Table'
 
 function MyApp() {
     const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            name: 'Dee',
-            job: 'Aspring actress',
-        },
-        {
-            name: 'Dennis',
-            job: 'Bartender',
-        },
+        // {
+        //     name: 'Charlie',
+        //     job: 'Janitor',
+        // },
+        // {
+        //     name: 'Mac',
+        //     job: 'Bouncer',
+        // },
+        // {
+        //     name: 'Dee',
+        //     job: 'Aspring actress',
+        // },
+        // {
+        //     name: 'Dennis',
+        //     job: 'Bartender',
+        // },
     ]);
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
             return i !== index
-          });
+            });
         setCharacters(updated);
     }
+
+    function updateList(person){
+        setCharacters([...characters, person]);
+    }
+        
     return (
         <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        <Form handleSubmit={updateList} />
         </div>
     );  
 }
