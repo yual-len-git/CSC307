f26deccf374a7d5c9d8def1bda1dc5bb00ec32b6
yual-len-git <allenyu90@gmail.com>
2021-09-29 13:41:32 -0700

    first

---
+++ HelloWorld/sample_backend.py
@@ -0,0 +1,44 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      }, 
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+
+
+@app.route('/users')
+def get_users():
+   return users
+
