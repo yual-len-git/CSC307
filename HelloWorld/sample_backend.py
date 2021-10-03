64f8143ea6654afa53213fdb3800cf70f595b441
yual-len-git <allenyu90@gmail.com>
2021-10-03 14:50:14 -0700

    part1 done

--- HelloWorld/sample_backend.py
+++ HelloWorld/sample_backend.py
@@ -63,14 +63,27 @@ users = {
 #       return ({})
 #    return users
 
-@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+@app.route('/users', methods=['GET', 'POST'])#, 'DELETE'])
 def get_users():
    if request.method == 'GET':
       search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if search_username and search_job:
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username and user['job'] == search_job:
+               subdict['users_list'].append(user)
+         return subdict
       if search_username :
          subdict = {'users_list' : []}
          for user in users['users_list']:
             if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      elif search_job:
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['job'] == search_job:
                subdict['users_list'].append(user)
          return subdict
       return users
@@ -78,18 +91,42 @@ def get_users():
       userToAdd = request.get_json()
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
    # elif request.method == 'DELETE':
    #    delete_username = request.args.get('name')
    #    if delete_username:
-   #       deldict = {'users_list' : []}
    #       for user in users['users_list']:
    #          if user['name'] == delete_username:
-   #             # deldict['users_list'].pop(user)
-   #             del deldict[user]
-   #       return deldict
+   #             users['users_list'].remove(user)
+   #       return users
    #    return users
 
+
+# @app.route('/users/<name>', methods=['DELETE'])
+# def get_user(name):
+#    if name :
+#       for user in users['users_list']:
+#         if user['name'] == name:
+#            if request.method == 'DELETE':
+#               users['users_list'].remove(user)
+#             #   resp = jsonify(success=True)
+#             #   resp.status_code = 204
+#               return users
+#       return users
+#    return users
+
+@app.route('/users/<id>', methods=['DELETE'])
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           if request.method == 'DELETE':
+              users['users_list'].remove(user)
+            #   resp = jsonify(success=True)
+            #   resp.status_code = 204
+              return users
+      return users
+   return users
 
