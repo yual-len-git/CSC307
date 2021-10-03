772bc03eb1c9c62b809c39ef28876cc6b052d2f5
yual-len-git <allenyu90@gmail.com>
2021-10-03 12:14:21 -0700

    changes

--- HelloWorld/sample_backend.py
+++ HelloWorld/sample_backend.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -36,9 +38,58 @@ users = {
    ]
 }
 
+# @app.route('/users')
+# def get_users():
+#    return users
 
 
-@app.route('/users')
+# @app.route('/users')
+# def get_users():
+#    search_username = request.args.get('name') #accessing the value of parameter 'name'
+#    if search_username :
+#       subdict = {'users_list' : []}
+#       for user in users['users_list']:
+#          if user['name'] == search_username:
+#             subdict['users_list'].append(user)
+#       return subdict
+#    return users
+
+# @app.route('/users/<id>')
+# def get_user(id):
+#    if id :
+#       for user in users['users_list']:
+#         if user['id'] == id:
+#            return user
+#       return ({})
+#    return users
+
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-   return users
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+   # elif request.method == 'DELETE':
+   #    delete_username = request.args.get('name')
+   #    if delete_username:
+   #       deldict = {'users_list' : []}
+   #       for user in users['users_list']:
+   #          if user['name'] == delete_username:
+   #             # deldict['users_list'].pop(user)
+   #             del deldict[user]
+   #       return deldict
+   #    return users
+
 
