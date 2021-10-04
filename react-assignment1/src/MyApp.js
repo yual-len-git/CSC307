61ce68114ea24c3eb5405e6afd335fabe24dd393
yual-len-git <allenyu90@gmail.com>
2021-10-03 23:21:04 -0700

    part2

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -1,45 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Table from './Table'
 import Form from './Form'
-
-// const characters = [
-//     {
-//       name: 'Charlie',
-//       job: 'Janitor',
-//     },
-//     {
-//       name: 'Mac',
-//       job: 'Bouncer',
-//     },
-//     {
-//       name: 'Dee',
-//       job: 'Aspring actress',
-//     },
-//     {
-//       name: 'Dennis',
-//       job: 'Bartender',
-//     },
-// ];
+import axios from 'axios'
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        // {
-        //     name: 'Charlie',
-        //     job: 'Janitor',
-        // },
-        // {
-        //     name: 'Mac',
-        //     job: 'Bouncer',
-        // },
-        // {
-        //     name: 'Dee',
-        //     job: 'Aspring actress',
-        // },
-        // {
-        //     name: 'Dennis',
-        //     job: 'Bartender',
-        // },
-    ]);
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchAll().then( result => {
+           if (result)
+              setCharacters(result);
+         });
+     }, [] );
+
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
             return i !== index
@@ -56,7 +29,19 @@ function MyApp() {
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
         <Form handleSubmit={updateList} />
         </div>
-    );  
+    ); 
+    
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+     }
 }
 
 
