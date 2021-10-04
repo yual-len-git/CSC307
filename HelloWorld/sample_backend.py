4b602fc810e4708cb3ca451a66e708d02838fa84
yual-len-git <allenyu90@gmail.com>
2021-10-03 19:01:41 -0700

    front and backend connected

--- HelloWorld/sample_backend.py
+++ HelloWorld/sample_backend.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
