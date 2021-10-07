0db419d3b3d171cbf3741d50a67af99a59815880
yual-len-git <allenyu90@gmail.com>
2021-10-06 17:17:31 -0700

    done?

--- Stock_Portfolio/stock_portfolio.py
+++ Stock_Portfolio/stock_portfolio.py
@@ -1,11 +1,4 @@
 
-
-# class stock:
-#     def __init__(self, name, quantity, price):
-#         self.name_of_stock = name
-#         self.quantity = quantity
-#         self.price = price
-
 class user:
     def __init__(self, name):
         self.user = name
@@ -13,18 +6,36 @@ class user:
     portfolio = {}
 
     def buy_stock(self, name, quantity):
-        if name not in self.portfolio:
+        if quantity <= 0:
+            raise QuantityError("Quantity can not be less than 1")
+        elif name not in self.portfolio:
             self.portfolio[name] = quantity
         else:
             self.portfolio[name] += quantity
             
     def sell_stock(self, name, quantity):
-        if name not in self.portfolio:
-            print('Stock not in profolio')
-            return
+        if quantity <= 0:
+            raise QuantityError("Quantity can not be less than 1")
+        elif name not in self.portfolio:
+            raise MissingError('Stock not in profolio')
         elif self.portfolio[name] < quantity:
-            print("Insufficient quantity of stock")
-            return
+            raise ShareSaleException("Insufficient quantity of stock")
         else:
             self.portfolio[name] -= quantity
-            return
+            if self.portfolio[name] == 0:
+                self.portfolio.pop(name)
+
+    def show_stock(self, name):
+        return self.portfolio[name]
+
+
+
+class ShareSaleException(Exception):
+    # print("Insufficient quantity of stock")
+    pass
+
+class QuantityError(Exception):
+    pass
+
+class MissingError(Exception):
+    pass
--- Stock_Portfolio/test.py
+++ Stock_Portfolio/test.py
@@ -1,4 +1,5 @@
 from stock_portfolio import *
+import pytest
 
 user1 = user('Allen')
 
@@ -6,16 +7,33 @@ def test_create():
     # user1 = user('Allen')
     assert len(user1.portfolio) == 0
 
-
 def test_stock_add():
     # user2 = user('Brandon')
     user1.buy_stock('gme', 5)
     user1.buy_stock('rblx', 10)
     assert len(user1.portfolio) == 2
 
-
 def test_buy_stock():
     user1.buy_stock('gme', 10)
     user1.buy_stock('rblx', 10)
     assert user1.portfolio['gme'] == 15
     assert user1.portfolio['rblx'] == 20
+
+def test_sell_stock():
+    user1.sell_stock('gme', 5)
+    user1.sell_stock('rblx', 5)
+    assert user1.portfolio['gme'] == 10
+    assert user1.portfolio['rblx'] == 15
+
+def test_amount():
+    assert user1.show_stock('gme') == 10
+    assert user1.show_stock('rblx') == 15
+
+def test_remove():
+    user1.sell_stock('gme', 10)
+    assert ('gme' not in user1.portfolio) == True
+    assert ('gme' in user1.portfolio) == False
+
+def test_exception():
+    with pytest.raises(ShareSaleException):
+        user1.sell_stock('rblx', 20)
